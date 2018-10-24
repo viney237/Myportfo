9be94afe9f4fe01a88cd0b308b10c03ca48363f0
viney237 <vinaykumar.pilla9@gmail.com>
2018-10-24 11:31:09 +0200

    Updates

---
+++ precache-manifest.55f1b928a1c87ef0259c73a559f98931.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Myportfo/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b1933e2abd9563185a54",
+    "url": "/Myportfo/static/js/runtime~main.b1933e2a.js"
+  },
+  {
+    "revision": "5948a1dd73a43199d0d9",
+    "url": "/Myportfo/static/js/main.5948a1dd.chunk.js"
+  },
+  {
+    "revision": "a700ff875c24dcc7b8f8",
+    "url": "/Myportfo/static/js/1.a700ff87.chunk.js"
+  },
+  {
+    "revision": "5948a1dd73a43199d0d9",
+    "url": "/Myportfo/static/css/main.de6e2782.chunk.css"
+  },
+  {
+    "revision": "1f04f0f400addb50b0078fee04e27358",
+    "url": "/Myportfo/index.html"
+  }
+];
